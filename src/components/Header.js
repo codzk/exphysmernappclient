c060b9c4ce7eaff13131d6bb0aea132d8780bebf
codzk <zekemilne@gmail.com>
2024-07-13 11:03:34 +1000

    feat: create header component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import './Header.css';
+import logo from '../assets/1.png'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="ExPhys Clinic Logo" />
+            </div>
+            <nav>
+                <ul>
+                    <li><Link to="/services">Our Services</Link></li>
+                    <li><a href="tel:0412345678">0412345678</a></li>
+                    <li><Link to= "/book">Book</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
