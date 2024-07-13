ea48b7a1ee5a3fafe7a394dd721761d0581400f0
codzk <zekemilne@gmail.com>
2024-07-13 12:36:30 +1000

    feat: installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </main>
+    </Router>
+    
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import './Header.css';
-import logo from '../assets/1.png'
+import logo from '../assets/logo.png'
 
 const Header = () => {
     return (
