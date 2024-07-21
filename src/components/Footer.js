bc5df535762cd65d398cbebd96802d6e84cf83b0
codzk <zekemilne@gmail.com>
2024-07-21 20:43:11 +1000

    feat: Added react-scroll to Footer for smooth scrolling to services section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './Footer.css';
 import footerLogo from '../assets/footerlogo.png';
+import { Link } from 'react-scroll';
+
 
 const Footer = () => {
     return (
@@ -9,10 +11,11 @@ const Footer = () => {
                 <img src={footerLogo} alt="ExPhys Clinic Logo" />
             </div>
             <div className="footer-links">
-                <a href="#services-section">Our Services</a>
+                <Link to="services-section" smooth={true} duration={500}>Our Services</Link>
                 <a href="tel:0412345678">0412345678</a>
-                <a href="#contact-section">Book</a>
+                <Link to="book-section" smooth={true} duration={500}>Book</Link>
                 <a href="#admin-login">Admin Login</a>
+
             </div>
             <div className="footer-bottom">
                 <p>&copy; ExPhys Clinic 2024. All Rights Reserved.</p>
