29d8aa1b8db795e6800fd90058f5a4f5691ba76d
codzk <zekemilne@gmail.com>
2024-08-08 01:36:14 +1000

    fix: not connecting (cors)

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Component, ...rest }) => {
-    const token = localStorage.getItem('token');
-    return token ? <Component {...rest} /> : <Navigate to="/admin-login" />;
+const ProtectedRoute = ({ element: Component }) => {
+  const token = localStorage.getItem('token');
+
+  return token ? <Component /> : <Navigate to="/admin-login" />;
 };
 
 export default ProtectedRoute;
--- src/axios.js
+++ src/axios.js
@@ -1,20 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001', // Update this with your backend URL
 });
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
