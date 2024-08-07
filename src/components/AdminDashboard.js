3df9372b7c202d8dd761df12b3e9afff52e3e91e
codzk <zekemilne@gmail.com>
2024-08-07 22:32:43 +1000

    fix: update AdminDashboard.js

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line no-unused-vars
 import { fetchAppointments, createAppointment, updateAppointment, deleteAppointment } from '../api';
 
 const AdminDashboard = () => {
