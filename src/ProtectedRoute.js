1fa0ac0f5ad0a75050f045e17ff22c48b6fac15a
codzk <zekemilne@gmail.com>
2024-08-08 11:05:29 +1000

    updated axios.js

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,11 +1,21 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
 import { Route, Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ element: Component, ...rest }) => {
-  const token = localStorage.getItem('token');
+    const isAuthenticated = !!localStorage.getItem('token');
 
-  return token ? <Component {...rest} /> : <Navigate to="/admin-login" />;
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isAuthenticated ? (
+                    <Component {...props} />
+                ) : (
+                    <Navigate to="/admin-login" />
+                )
+            }
+        />
+    );
 };
 
 export default ProtectedRoute;
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api';
+
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001', // Update this with your backend URL
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default api;
