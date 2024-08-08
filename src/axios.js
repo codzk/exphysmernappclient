2ef83717d631910ddc6a9cf3ee9ecec36f1e5c71
codzk <zekemilne@gmail.com>
2024-08-08 11:57:35 +1000

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 // Create an instance of axios
 const api = axios.create({
